ef2a8e1e5659c6c886e66a43cc66eb89e7272308
Sushant Bhaiswar <96516472+SushantBhaiswar@users.noreply.github.com>
2023-01-27 22:53:23 +0530

    cyclic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const route = require('./routes/route');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require("cors")
+const path = require("path");
 app.use(express.json());
 require("dotenv").config()
 
@@ -15,24 +16,28 @@ mongoose.connect("mongodb+srv://Sushant_Bhaiswar_30:WBYUu1bCYmxmZUmg@cluster0.ju
 app.use(cors())
 app.use('/', route)
 
+app.use(express.static(path.join(__dirname, "./client/build")));
 
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 //vercel 
 
-if (process.env.NODE_ENV == 'production') {
-    const path = require('path')
-    app.use(express.static(path.join(__dirname, "../client/build")));
+// if (process.env.NODE_ENV == 'production') {
+//     const path = require('path')
+//     app.use(express.static(path.join(__dirname, "../client/build")));
 
-    app.get("*", function (_, res) {
-        res.sendFile(
-            path.join(__dirname, "../client/build/index.html"),
-            function (err) {
-                if (err) {
-                    res.status(500).send(err)
-                }
-            }
-        )
-    })
-}
+//     app.get("*", function (_, res) {
+//         res.sendFile(
+//             path.join(__dirname, "../client/build/index.html"),
+//             function (err) {
+//                 if (err) {
+//                     res.status(500).send(err)
+//                 }
+//             }
+//         )
+//     })
+// }
 
 app.listen(process.env.PORT || 3001, function () {
     console.log('Express app running on port ' + 3001)
