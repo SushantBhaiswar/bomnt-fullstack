161c5296b808e2a61fee58a3fafaf33ac462aca6
sushant <sb.bhaiswar@gmail.com>
2024-09-25 23:25:55 +0530

    feat : login and header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -24,24 +24,14 @@ function Header() {
     }
     return (
         <div className='main-header'>
-            <div className="menue-items">
+            <div className={`menue-items${isHamburgerOpen ? 'open': ''}`}>
                 <p>My Orders</p>
                 <p>My Appointments</p>
                 <p>My Payments</p>
                 <p>Contact Us</p>
             </div>
 
-            {/* <div className='menue-open'  >
-                <input type="text"
-                    placeholder="Search"
-                />
-                <div className="hamburger" style={{ cursor: 'pointer', marginLeft: '10px' }}>
-                    <a href="javascript:void(0);" onClick={handleHamburger}>
-                        <i class="fa fa-bars"></i>
-                    </a>
 
-                </div>
-            </div>   */}
 
             <div className="searchBox">
                 <input className="inputinput" type="text"
@@ -49,21 +39,30 @@ function Header() {
                 // style={{ width: '530px',  padding: '0 10px', height: '40px', 'border-radius': '5px' }}
                 />
             </div>
-            <div style={{ cursor: 'pointer', marginLeft: '20px' }}>
+            <div className='menue-open'  >
+
+                <div className="hamburger" style={{ cursor: 'pointer', marginLeft: '10px' }}>
+                    <a href="javascript:void(0);" onClick={handleHamburger}>
+                        <i class="fa fa-bars" style={{ fontSize: '42px' }}></i>
+                    </a>
+
+                </div>
+            </div>
+
+            <div className="logout-profile" >
                 <img
                     src="/ic_logout.8bcac3ce.png"
                     alt="Logout"
                     onClick={handleLogoutModel}
                     style={{ width: '40px', height: '40px' }}
                 />
-            </div>
-            <div style={{ cursor: 'pointer', marginLeft: '20px' }}>
                 <img
                     src="profilePlaceholderIcon.3785a8ea.svg"
                     alt="Profile"
                     style={{ width: '40px', height: '40px' }}
                 />
             </div>
+
 
             <Modal isOpen={isModelOpen} onClose={handleLogoutModel} message="Are you sure you want to logout?"
                 title="Confirm Logout">
